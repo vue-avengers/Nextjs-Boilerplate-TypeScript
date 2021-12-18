5a910d0bb2a769a4a29103c6680508d112c96ece
jsdeveloper <jsdeveloperr@gmail.com>
2021-12-18 10:35:53 +0300

    style lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
+  '*.{css,scss}': ['stylelint --fix', 'stylelint'],
   '**/*.ts?(x)': () => 'npm run build-types',
   '*.json': [
     'prettier --write --config .prettierrc.json {src,e2e,docs}/**/*.{ts,tsx}',
