6ff61211a7ca5b85da626dcc66ebce1e90f71993
Büşra Şanlıbayrak <busrasanlibayrak@Busra-sanlibayrak-MacBook-Pro.local>
2021-12-19 21:55:56 +0300

    build fix

--- src/stores/context/global-context.tsx
+++ src/stores/context/global-context.tsx
@@ -5,13 +5,16 @@ import React, { createContext, useContext } from 'react';
 // const GlobalContext = createContext<Partial<IContext>>({});
 const GlobalContext = createContext({} as any);
 
-export function GlobalProvider({ children, value }: any) {
+export function GlobalProvider({ children }: { children: any }) {
   return (
-    <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
+    <GlobalContext.Provider value={children?.props}>
+      {children}
+    </GlobalContext.Provider>
   );
 }
 
 export function useGlobalContext() {
   const context = useContext(GlobalContext);
+
   return context;
 }
